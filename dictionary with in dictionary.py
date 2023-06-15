05a28d19ed9ce8b329f3c496a656f867edbf3f1b
Amar Narote <amar.narote01@gmail.com>
2023-06-15 10:09:32 +0530

    .

--- dictionary with in dictionary.py
+++ dictionary with in dictionary.py
@@ -29,11 +29,34 @@ D={'emp1':{'name':'Bob','job':'Manager','sal':45000},
    'emp2':{'name':'kim','job':'Developer','sal':10000},
    'emp3':{'name':'sam','job':'Admin','sal':30000},
    'emp4':{'name':'John','job':'Admin','sal':35000},
-   'emp5':{'name':'Martin','job':'Manager','sal':45000},
+   'emp5':{'name':'Martin','job':'Manager','sal':55000},
    'emp6':{'name':'blake','job':'Developer','sal':20000}
    }
-for key,val in D.items():
-    for k,v in val.items():
-        if k=='job' and v=='Admin':
-            print(f"{key}:-{val}")
-            print("----------------")
+# for key,val in D.items():
+#     for k,v in val.items():
+#         if k=='job' and v=='Admin':
+#             print(f"{key}:-{val}")
+#             print("----------------")
+# print("highest salary")
+# max=0
+# for key,val in D.items():
+#     for k,v in val.items():
+#         if k=='sal':
+#             if max < v:
+#                 max=v
+# for key,val in D.items():
+#     for k,v in val.items():
+#         if v==max:
+#             print(key,val)  
+# ------------------------------------------------
+# add key value pair from user 
+
+key=input("enter key ")    
+val={}
+n=int(input("enter no of key value pair"))
+for i in range(1,n+1):
+    k=input("enter key")
+    v=input("enter val")
+    val.update({k:v})
+D.update({key:val})
+print(D)
